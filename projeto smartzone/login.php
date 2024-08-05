cdf474d2de7930ffa892310b1af03882d7e98f28
Rafael rodrigues <120226602+RafaelElsuke@users.noreply.github.com>
2024-08-05 09:15:40 -0300

    Add files via upload

--- projeto smartzone/login.php
+++ projeto smartzone/login.php
@@ -29,6 +29,7 @@
     <script src="https://kit.fontawesome.com/2efb6f99a5.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="cadlog.css">
+    <link href="css/global.css" rel="stylesheet" />
     <title>Login</title>
 </head>
 <body>
@@ -120,10 +121,7 @@
 
    
   <!-- Footer -->
-  <footer
-          class="text-center text-lg-start text-white"
-          style="background-color: #45526e"
-          >
+  <footer id="rede"class="text-center text-lg-start text-white">
     <!-- Grid container -->
     <div class="container p-4 pb-0">
       <!-- Section: Links -->
