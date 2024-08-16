af03c12dd8320e1e61f1863198d44ff14cbc1939
Alana Leitielly <143668815+leitielly@users.noreply.github.com>
2024-08-16 08:48:00 -0300

    Delete PROJETO FINAL WEB directory

--- PROJETO FINAL WEB/smartzone/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "Rafael";
-$senha = "123456";
-$dbname = "cadastro_loja";
-
-//cria conexäo
-$conn = mysqli_connect("$servidor","$usuario","$senha","$dbname");
